348dbec7951d3aa07e93b2405866444dc41db365
subham46 <itshub46>
2020-06-04 18:31:55 +0530

    Modifiaction in Save method

--- src/main/java/com/hexagonal/kata/katahexa/domain/domainservice/DepositService.java
+++ src/main/java/com/hexagonal/kata/katahexa/domain/domainservice/DepositService.java
@@ -4,6 +4,8 @@ import com.hexagonal.kata.katahexa.domain.domainentity.Account;
 import com.hexagonal.kata.katahexa.domain.port.outport.SaveAccountPort;
 import com.hexagonal.kata.katahexa.domain.port.inport.DepositInPort;
 import com.hexagonal.kata.katahexa.domain.port.outport.LoadAccountPort;
+import com.hexagonal.kata.katahexa.infrastucture.TransactionType;
+import com.hexagonal.kata.katahexa.infrastucture.UserAccount;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import java.math.BigDecimal;
@@ -20,12 +22,14 @@ public class DepositService implements DepositInPort {
 
     @Override
     public void deposit(Long accountId, BigDecimal amount) {
-        Account account = depositOutPort.getAccount(accountId);
+        UserAccount account = depositOutPort.getAccount(accountId);
         account.setBalance(account.getBalance().add(amount));
-        save(account);
+        Account ac = new Account(account.getAccountId(),account.getName(),account.getBalance());
+        save(ac,amount, TransactionType.CREDIT);
     }
 
-    public void save(Account account){
-        createAccountOutPort.create(account);
+    public void save(Account account,BigDecimal amount,TransactionType t){
+
+        createAccountOutPort.create(account,amount,t);
     }
 }
